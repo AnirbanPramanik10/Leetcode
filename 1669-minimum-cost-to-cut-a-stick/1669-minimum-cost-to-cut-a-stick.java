e415281ac58f4d3be30e07c04ced7d3f4654c558
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-08-23 10:07:27 +0530

    Time: 11 ms (90.16%), Space: 41.6 MB (37.05%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,33 +1,20 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
-        int temp[] = new int[cuts.length+2];
-        temp[0] = 0;
-        
-        for(int i = 1; i< temp.length-1; i++){
-            temp[i] = cuts[i-1];
-        }
-        
-        temp[temp.length-1] = n;
+        int[] temp = new int[cuts.length + 2];
+        System.arraycopy(cuts, 0, temp, 1, cuts.length);
+        temp[temp.length - 1] = n;
         Arrays.sort(temp);
-        
-        int dp[][] = new int[temp.length+2][temp.length+2];
-        
-        for(int a[]: dp)
-            Arrays.fill(a, 0);
-        
-        for(int i = cuts.length; i >= 1; i--){
-            for(int j = 1; j <= cuts.length; j++){
-                if(i > j)
-                    continue;
-                int minCost = Integer.MAX_VALUE;
-                for(int ind = i; ind <= j; ind++){
-                    int cost = temp[j+1] - temp[i-1] + dp[i][ind-1] + dp[ind+1][j];
-                    minCost = Math.min(cost, minCost);
-                }
-                dp[i][j] = minCost;
+
+        int[][] dp = new int[temp.length][temp.length];
+
+        for (int len = 2; len < temp.length; len++)
+            for (int i = 0; i + len < temp.length; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++)
+                    dp[i][j] = Math.min(dp[i][j], temp[j] - temp[i] + dp[i][k] + dp[k][j]);
             }
-        }
-        
-        return dp[1][cuts.length];
+
+        return dp[0][temp.length - 1];
     }
 }
