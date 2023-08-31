cc9efe22b4dc8b1285e218a96e10269fa8ae5b40
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-08-31 06:52:02 +0530

    Time: 3 ms (98.79%), Space: 44.4 MB (27.53%) - LeetHub

--- 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1451-minimum-number-of-taps-to-open-to-water-a-garden/1451-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -3,16 +3,11 @@ class Solution {
         int[] maxReach = new int[n+1];
         
         for(int i=0;i<ranges.length;i++) {
-            int s = Math.max(0, i - ranges[i]);
-            int e = i + ranges[i];
-            
+            int s = Math.max(0, i - ranges[i]), e = i + ranges[i];   
             maxReach[s] = e;
         }
         
-        int tap = 0;
-        int currEnd = 0;
-        int nextEnd = 0;
-        
+        int tap = 0, currEnd = 0, nextEnd = 0;     
         for(int i=0;i<=n;i++) {
             if(i > nextEnd) return -1;
             if(i > currEnd) {
@@ -21,7 +16,6 @@ class Solution {
             }
             nextEnd = Math.max(nextEnd, maxReach[i]);
         }
-        
         return tap;
     }
 }
