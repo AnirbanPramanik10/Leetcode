d96d45aea5007601ee275bac1299d989dfc5805d
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-09-01 08:33:51 +0530

    Time: 11 ms (13.02%), Space: 48.8 MB (6.44%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] countBits(int n) {
-        int arr[] = new int[n+1];
+        int arr[]=new int[n+1];
         for(int i=0;i<=n;i++){
-            String bin = Integer.toBinaryString(i);
-            long t = Long.parseLong(bin);
-            int count = 0;
-
-            while(t > 0){
-                long rem = t % 10;
-                if(rem == 1)count++;
-                t = t / 10;
-            }
-            arr[i] = count;
+            arr[i] = getBinaryOnes(i);
         }
         return arr;
+    }
+    public int getBinaryOnes(int i){
+        String binary = Integer.toBinaryString(i);
+        int count=0;
+        for(int j=0;j<binary.length();j++){
+            if(binary.charAt(j) == '1') count++;
+        }
+        return count;
     }
 }
