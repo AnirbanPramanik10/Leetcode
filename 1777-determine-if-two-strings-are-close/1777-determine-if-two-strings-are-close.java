1fde25ca1f87ee6031dde260e3dde3604b571734
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-09-03 15:20:25 +0530

    Time: 9 ms (98.72%), Space: 44.8 MB (23.64%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,20 +1,23 @@
 class Solution {
-    public boolean closeStrings(String x, String y) {
-        if(x.length()!=y.length()) return false;
-        int frq1[] = new int[26];
-        int frq2[] = new int[26];
-        Set<Character> s1 = new HashSet<>();
-        Set<Character> s2 = new HashSet<>();
-        for(char ch : x.toCharArray()){
-            frq1[ch-'a']++;
-            s1.add(ch);
-        }
-        for(char ch : y.toCharArray()){
-            frq2[ch-'a']++;
-            s2.add(ch);
-        }
-        Arrays.sort(frq1);
-        Arrays.sort(frq2);
-        return Arrays.equals(frq1,frq2) && s1.equals(s2);
+    public boolean closeStrings(String word1, String word2) {
+        if (word1.length() != word2.length()) return false;
+        if (word1.equals(word2)) return true;
+        
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        
+        for (char c : word1.toCharArray()) count1[c - 'a']++;
+        for (char c : word2.toCharArray()) count2[c - 'a']++;
+        
+        for (int i = 0; i < 26; i++)
+            if ((count1[i] == 0 && count2[i] > 0) || (count1[i] > 0 && count2[i] == 0))
+                return false;
+        
+        Arrays.sort(count1);
+        Arrays.sort(count2);
+        
+        for (int i = 0; i < 26; i++)
+            if (count1[i] != count2[i]) return false;
+        return true;
     }
 }
