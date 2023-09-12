08936d02755bac11340424cb4a07a5b5db709fe7
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-09-12 09:38:48 +0530

    Time: 7 ms (98.85%), Space: 44.5 MB (65.1%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,18 +1,44 @@
 class Solution {
     public int minDeletions(String s) {
-        int ans = 0;
-        int[] cnt = new int[26]; 
-        for (char c : s.toCharArray()) {
-            cnt[c - 'a']++;
+        int[] freq = new int[26];
+        for( char ch: s.toCharArray()){
+            freq[ch-'a']++;
         }
-        
-        Set<Integer> set = new HashSet<>(); 
-        for (int freq : cnt) {
-            while (freq > 0 && !set.add(freq)) {
-                freq--;
-                ans++;
+
+        Arrays.sort(freq);
+        reverse(freq);
+        int prev = freq[0];
+        int i = 1, n = s.length(), del = 0;
+
+        // for(int f: freq){
+        //     System.out.print(f+" ");
+        // }
+        // System.out.println();
+
+        while( i < 26 && freq[i]>0){
+            while(freq[i]>=prev && freq[i]>0){
+                freq[i]--;
+                del++;          
             }
-        }    
-        return ans;
+            prev = freq[i];
+            i++;
+        }
+        return del;      
+    }
+
+    public void reverse(int[] arr){
+        int i = 0; 
+        int j = arr.length -1;
+        while( i<j){
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public void swap( int[] arr , int i , int j){
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
     }
 }
