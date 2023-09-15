1d77f58c0657ed0d679a4f9c037196cf3e393372
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-09-15 11:55:28 +0530

    Time: 587 ms (18.73%), Space: 66.2 MB (33.73%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,46 +1,33 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        List<int[]> edges = new ArrayList<>();
-        for (int i = 0; i < points.length - 1; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                edges.add(new int[] {i, j, dist});
-            }
+        PriorityQueue<int[]> edges = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        int n = points.length;
+        Set<Integer> visited = new HashSet<>();
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            edges.add(new int[]{0, j, dist(points[0], points[j])});
         }
-        // Sort by weight
-        Collections.sort(edges, (a, b) ->Integer.compare(a[2], b[2]));
-        // Use DSU to get parent
-        int[] parents = new int[points.length];
-        for (int i = 0; i < parents.length; i++) {
-            parents[i] = i;
-        }
-        // Add edges that aren't creating a loop
-        int mstTotal = 0;
-        int edgeUsed = 0;
-        for (int[] edge : edges) {
-            int i = edge[0];
-            int j = edge[1];
-            int parentI = findParent(parents, i);
-            int parentJ = findParent(parents, j);
-            if (parentI != parentJ) {
-                // Union
-                parents[parentI] = parentJ;
-                mstTotal += edge[2];
-                edgeUsed++;
-                if (edgeUsed == points.length - 1) {
-                    break;
+        int edgeCount = 0;
+        visited.add(0);
+        int res = 0;
+        while (!edges.isEmpty() && edgeCount < n) {
+            int[] curr = edges.poll();
+            int p1 = curr[0], p2 = curr[1], weight = curr[2];
+            if (!visited.contains(p2)) {
+                visited.add(p2);
+                edgeCount++;
+                if (edgeCount >= n) break;
+                res += weight;
+                for (int j = 0; j < n; j++) {
+                    if (!visited.contains(j))
+                        edges.add(new int[] {p2, j, dist(points[p2], points[j])});
                 }
             }
         }
-        return mstTotal;
+        return res;
     }
-    
-    int findParent(int[] parents, int i) {
-        if (parents[i] == i) {
-            return i;
-        }
-        int parent = findParent(parents, parents[i]);
-        parents[i] = parent;
-        return parent;
+
+    private int dist(int[] p1, int[] p2) {
+        return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
     }
 }
