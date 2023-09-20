b9dc0cbfcfb8c592ade6ba497db4c23dc0f01e3a
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-09-20 14:13:01 +0530

    Time: 66 ms (17.5%), Space: 59.3 MB (54.44%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
@@ -1,20 +1,22 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int target=-x;
-        for(int i=0;i<nums.length;i++){
-            target+=nums[i];
-        }
-        if(target==0) return nums.length;
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        int sum=0,res=-1;
-        hs.put(0,-1);
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-            if(hs.containsKey(sum-target)){
-                res=Math.max(res,i-hs.get(sum-target));
+        int n = nums.length;
+        int target = -x;
+        if (target == 0) return 0;
+        for (int num : nums) target += num;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0, res = -1;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            map.put(sum, i);
+            
+            if (map.containsKey(sum - target)) {
+                res = Math.max(res, i - map.get(sum - target));
             }
-            hs.put(sum,i);
         }
-        return res==-1?-1:nums.length-res;
+
+        return res == -1 ? res : n - res;
     }
 }
