2289d1cffb1d801fe6abd2272700337954186521
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-10-01 10:56:42 +0530

    Time: 6 ms (60.53%), Space: 44.1 MB (76.51%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,5 @@
+class Solution {
+    public String reverseWords(String s) {
+        return Arrays.stream(s.split(" ")).map(word -> new StringBuilder(word).reverse().toString()).collect(Collectors.joining(" "));
+    }
+}
