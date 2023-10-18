c9edb94408ddbeb2817043867de0b49c38585446
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-10-18 09:21:50 +0530

    Time: 17 ms (94.67%), Space: 72.7 MB (27.56%) - LeetHub

--- 2176-parallel-courses-iii/2176-parallel-courses-iii.java
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.java
@@ -1,32 +1,29 @@
 class Solution {
     public int minimumTime(int n, int[][] relations, int[] time) {
-        ArrayList<Integer>[] graph = new ArrayList[n];
-        int[] inDegree = new int[n];
-        int[] dp = new int[n];
-        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
-        for (int[] r : relations) {
-            graph[r[0] - 1].add(r[1] - 1);
-            inDegree[r[1] - 1]++;
+        List<Integer>[] graph = new List[n + 1];
+        int[] minTime = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+        for (int[] relation : relations) graph[relation[1]].add(relation[0]);
+
+        for (int i = 1; i <= n; i++) {
+            if (minTime[i] > 0) continue;
+            dfs(i, graph, time, minTime);
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < n; i++)
-            if (inDegree[i] == 0) {
-                q.offer(i);
-                dp[i] = time[i];
-            }
-
-        int minTime = 0;
-        while (!q.isEmpty()) {
-            int course = q.poll();
-            minTime = Math.max(minTime, dp[course]);
-            for (int nextCourse : graph[course]) {
-                inDegree[nextCourse]--;
-                dp[nextCourse] = Math.max(dp[nextCourse], dp[course] + time[nextCourse]);
-                if (inDegree[nextCourse] == 0) q.offer(nextCourse);
-            }
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(max, minTime[i]);
         }
+        return max;
+    }
 
-        return minTime;
+    private void dfs(int start, List<Integer>[] graph, int[] time, int[] minTime) {
+        int max = 0;
+        for (int next : graph[start]) {
+            if (minTime[next] == 0) dfs(next, graph, time, minTime);
+            max = Math.max(max, minTime[next]);
+        }
+        minTime[start] = max + time[start - 1];
     }
 }
