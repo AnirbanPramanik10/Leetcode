2ca03c5ac8f6801006b696f49caf9a8d3d294650
Anirban Pramanik <122162797+AnirbanPramanik10@users.noreply.github.com>
2023-10-28 10:51:10 +0530

    Time: 6 ms (93.4%), Space: 39.3 MB (82.18%) - LeetHub

--- 1332-count-vowels-permutation/1332-count-vowels-permutation.java
+++ 1332-count-vowels-permutation/1332-count-vowels-permutation.java
@@ -1,17 +1,22 @@
 class Solution {
     public int countVowelPermutation(int n) {
-        int MOD = 1000000007;
-        long[][] dp = new long[n + 1][5];
-        for (int i = 0; i < 5; i++) dp[1][i] = 1;
-        for (int i = 2; i <= n; i++) {
-            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % MOD;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
-            dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % MOD;
-            dp[i][3] = dp[i - 1][2];
-            dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % MOD;
+        if (n == 1) return 5;
+    
+        long mod = 1_000_000_007;
+        long a = 1, e = 1, i = 1, o = 1, u = 1;
+        long a1 = 0, e1 = 0, i1 = 0, o1 = 0, u1 = 0;
+        for (int j = 1; j < n; j++) {
+            a1 = (e + i + u) % mod;
+            e1 = (a + i) % mod;
+            i1 = (e + o) % mod;
+            o1 = i;
+            u1 = (i + o) % mod;
+            a = a1;
+            e = e1;
+            i = i1;
+            o = o1;
+            u = u1;
         }
-        long total = 0;
-        for (int i = 0; i < 5; i++) total = (total + dp[n][i]) % MOD;
-        return (int) total;
+        return (int) ((a + e + i + o + u) % mod);
     }
 }
